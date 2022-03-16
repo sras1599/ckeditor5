bdd16851f77d51539338172ee1b6ddcf617b11bd
Raspreet Singh <raspreet@xtenav.com>
2022-03-17 01:54:22 +0530

    Add the 'listProperties' plugin to the ckeditor build

--- packages/ckeditor5-build-inline/src/ckeditor.js
+++ packages/ckeditor5-build-inline/src/ckeditor.js
@@ -40,6 +40,7 @@ import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
+import ListProperties from '@ckeditor/ckeditor5-list/src/listproperties';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
@@ -81,6 +82,7 @@ InlineEditor.builtinPlugins = [
 	Indent,
 	Link,
 	List,
+  ListProperties,
 	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
